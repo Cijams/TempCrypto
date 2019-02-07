721bc23e3a3252d64f0fe6479c4c6777cc0a8850
Christopher <Cocopelly255@gmail.com>
2019-02-06 18:36:17 -0800

    Clarified Unit Testing Code
    Ensured each section had comments explaining their purpose.

--- crypto_unit_testing.py
+++ crypto_unit_testing.py
@@ -3,6 +3,11 @@ import pyCryptoFrost
 from binascii import hexlify
 from binascii import unhexlify
 
+"""
+  Unit Testing to instantiate and use the various functions
+  of the module.
+"""
+
 
 class MyTestCase(unittest.TestCase):
     master_key_256 = ""
@@ -10,14 +15,14 @@ class MyTestCase(unittest.TestCase):
 
     def setUp(self):
         """
-         Generate unencrypted text and master keys
+         Generate unencrypted text and master keys.
         """
         MyTestCase.master_key_256 = pyCryptoFrost.generate_master_key('sha256')
         MyTestCase.master_key_512 = pyCryptoFrost.generate_master_key('sha512')
 
     def test_master_keys(self):
         """
-          Generate and test master keys
+          Generate and test master keys.
          """
         # test keys are correct sizes
         self.assertEqual(32, len(MyTestCase.master_key_256))
@@ -92,14 +97,17 @@ class MyTestCase(unittest.TestCase):
         """
           Generate and test injection vectors
          """
+        # 16 bit IV.
         iv16 = pyCryptoFrost.generate_iv(16)
         print("\nIV 16:")
         print(hexlify(iv16).decode())
 
+        # 32 bit IV.
         iv32 = pyCryptoFrost.generate_iv(32)
         print("IV 32:")
         print(hexlify(iv32).decode())
 
+        # 56 bit IV.
         iv56 = pyCryptoFrost.generate_iv(56)
         print("IV 56:")
         print(hexlify(iv56).decode())
@@ -112,31 +120,46 @@ class MyTestCase(unittest.TestCase):
         self.assertNotEqual(iv56, pyCryptoFrost.generate_iv(56))
 
     def test_encrypt_aes(self):
-        r = b'**ENCRYPT ME AES**'
+        """
+        Test the AES256 and AES512 encryption algorithms
+        for correctness, uniqueness and functionality.
 
-        # Tests AES256
+        Tests decryption as well, ensures the text can
+        make the round trip.
+        """
+        text = b'**ENCRYPT ME AES**'
+
+        # Tests AES256.
         master_key = pyCryptoFrost.generate_master_key('sha256')
         key256 = pyCryptoFrost.generate_encryption_key('sha256', master_key)
-        pyCryptoFrost.encrypt_aes(r, key256, "aes128")
+        pyCryptoFrost.encrypt_aes(text, key256, "aes128")
         self.assertEqual(len(key256), 16)
 
-        # Tests AES512
+        # Tests AES512.
         master_key = pyCryptoFrost.generate_master_key('sha512')
         key512 = pyCryptoFrost.generate_encryption_key('sha512', master_key)
-        pyCryptoFrost.encrypt_aes(r, key512, "aes256")
+        pyCryptoFrost.encrypt_aes(text, key512, "aes256")
         self.assertEqual(len(key512), 32)
 
-        # Tests default encryption with dynamic assignments
-        print(pyCryptoFrost.encrypt_aes(r))
+        # Tests default encryption with dynamic assignments.
+        print(pyCryptoFrost.encrypt_aes(text))
 
         pyCryptoFrost.decrypt()
         pyCryptoFrost.decrypt("ff08c21b4c7045a64defca6136244fbf")
         pyCryptoFrost.decrypt("cdf042cefb0cffeda749da3c2a4bc137RRR")
 
     def test_encrypt_3des(self):
-        r = b'**ENCRYPT ME 3DES**'
+        """
+        Test the 3DES encryption algorithm for correctness,
+        uniqueness and functionality.
 
-        pyCryptoFrost.encrypt_3des(r)
+        Tests decryption as well, ensures the text can
+        make the round trip.
+        """
+        text = b'**ENCRYPT ME 3DES**'
+
+        # Tests 3DES.
+        pyCryptoFrost.encrypt_3des(text)
         pyCryptoFrost.decrypt()
         pyCryptoFrost.decrypt("ff08c21b4c7045a64defca6136244fbf")
         pyCryptoFrost.decrypt("cdf042cefb0cffeda749da3c2a4bc137RRR")
@@ -146,17 +169,22 @@ class MyTestCase(unittest.TestCase):
         self.assertGreater(len(test), 0)
 
     def test_generate_hmac(self):
-        r = b'**HMAC TEST**'
+        """
+        Ensures HMAC can be properly created and used.
+        """
+        text = b'**HMAC TEST**'
 
         master_key = pyCryptoFrost.generate_master_key('sha512')
         hmac_key = pyCryptoFrost.generate_hmac_key(16, master_key)
         print("hmac key:")
         print(hmac_key)
         print()
-        test = pyCryptoFrost.generate_hmac(hmac_key, r)
+        test = pyCryptoFrost.generate_hmac(hmac_key, text)
 
         self.assertGreater(len(test), 0)
         self.assertGreater(len(hmac_key), 0)
+
+# Start for unittest.
 
 
 if __name__ == '__main__':
